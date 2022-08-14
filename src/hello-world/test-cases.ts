504a8e2fa47b4a30b597eff0acaff781f61901b5
linguanghui <1476465526@qq.com>
2022-08-14 11:49:16 +0800

    add: 实现原来工具类型: Pick

--- src/hello-world/test-cases.ts
+++ src/hello-world/test-cases.ts
@@ -1,3 +1,4 @@
 import type { Equal, Expect, NotAny } from "@type-challenges/utils";
+import { HelloWorld } from './template';
 
 type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
